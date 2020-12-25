a55889ae22ecfd686a1ad133e104cafa851b0a3a
hiramatsu136 <hiramatsu136@adminnoMacBook-Pro.local>
2020-12-25 22:54:45 +0900

    Add form parameter.

--- pkg/httpserver/simpleserver.go
+++ pkg/httpserver/simpleserver.go
@@ -16,6 +16,7 @@ func HTTPServer() {
 	}
 	fmt.Println(port)
 	http.HandleFunc("/", IndexHandler)
+	http.HandleFunc("/setupsi", IndexHandler)
 	http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
 }
 
@@ -35,4 +36,29 @@ func IndexHandler(
 	} else {
 		fmt.Fprintf(w, "POSTはダメよ")
 	}
+}
+
+// setupSIHandler リクエストハンドラ
+func setupSIHandler(
+	w http.ResponseWriter,
+	r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		log.Fatalf("ParseForm error: %v", err)
+	}
+	// Destination はデプロイ先環境名です
+	var Destination string
+	// FrontEndHost はフロントエンドのホストアドレスです
+	var FrontEndHost string
+	// BackEndHost はバックエンドAPIのホストアドレスです
+	var BackEndHost string
+
+	Destination = r.Form.Get("dstmode")
+	FrontEndHost = r.Form.Get("FrontEndHost")
+	BackEndHost = r.Form.Get("BackEndHost")
+
+	fmt.Printf("Destination     : %s¥n", Destination)
+	fmt.Printf("FrontEndHost     : %s¥n", FrontEndHost)
+	fmt.Printf("BackEndHost     : %s¥n", BackEndHost)
+
 }
